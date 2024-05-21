7fd29c6fc7f440424894fb314c877ae9c588e628
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-21 21:58:08 +0400

    could not even do the custom register somethings wrong could not find the issue for like 30 minutes could be the clerk problem too

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -6,58 +6,61 @@ import { useRouter } from 'next/navigation'
 const RegisterPage = () => {
   const { isLoaded, signUp, setActive } = useSignUp()
   const [email, setEmail] = useState('')
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
   const [password, setPassword] = useState('')
   const [pendingVerification, setPendingVerification] = useState(false)
   const [code, setCode] = useState('')
   const router = useRouter()
 
   // Form Submit
-  const handleSubmit = async (e) => {}
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (!isLoaded) return
+    try {
+      await signUp.create({
+        emailAddress: email,
+        password,
+      })
+
+      await signUp.prepareEmailAddressVerification({
+        strategy: 'email_code',
+      })
+
+      setPendingVerification(true)
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
 
   // Verify User Email Code
-  const onPressVerify = async (e) => {}
+  const onPressVerify = async (e) => {
+    e.preventDefault()
+    if (!isLoaded) {
+      return
+    }
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code,
+      })
+      if (completeSignUp.status !== 'complete') {
+        console.log(JSON.stringify(completeSignUp, null, 2))
+      }
+      if (completeSignUp.status === 'complete') {
+        await setActive({ session: completeSignUp.createdSessionId })
+        router.push('/')
+      }
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2))
+    }
+  }
 
   return (
-    <div className='border p-5 rounded' style={{ width: '500px' }}>
+    <>
       {!pendingVerification && (
-        <div className=''>
+        <div className='w-[500px]'>
           <h1 className='text-2xl mb-4'>Register</h1>
           {!pendingVerification && (
             <form onSubmit={handleSubmit} className='space-y-4 md:space-y-6'>
-              <div>
-                <label
-                  htmlFor='first_name'
-                  className='block mb-2 text-sm font-medium text-gray-900'
-                >
-                  First Name
-                </label>
-                <input
-                  type='text'
-                  name='first_name'
-                  id='first_name'
-                  onChange={(e) => setFirstName(e.target.value)}
-                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5'
-                  required={true}
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor='last_name'
-                  className='block mb-2 text-sm font-medium text-gray-900'
-                >
-                  Last Name
-                </label>
-                <input
-                  type='text'
-                  name='last_name'
-                  id='last_name'
-                  onChange={(e) => setLastName(e.target.value)}
-                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5'
-                  required={true}
-                />
-              </div>
               <div>
                 <label
                   htmlFor='email'
@@ -69,6 +72,7 @@ const RegisterPage = () => {
                   type='email'
                   name='email'
                   id='email'
+                  value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5'
                   placeholder='name@company.com'
@@ -99,28 +103,29 @@ const RegisterPage = () => {
               </button>
             </form>
           )}
-          {pendingVerification && (
-            <div>
-              <form className='space-y-4 md:space-y-6'>
-                <input
-                  value={code}
-                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5'
-                  placeholder='Enter Verification Code...'
-                  onChange={(e) => setCode(e.target.value)}
-                />
-                <button
-                  type='submit'
-                  onClick={onPressVerify}
-                  className='w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
-                >
-                  Verify Email
-                </button>
-              </form>
-            </div>
-          )}
         </div>
       )}
-    </div>
+
+      {pendingVerification && (
+        <div>
+          <form className='space-y-4 md:space-y-6'>
+            <input
+              value={code}
+              className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5'
+              placeholder='Enter Verification Code...'
+              onChange={(e) => setCode(e.target.value)}
+            />
+            <button
+              type='submit'
+              onClick={onPressVerify}
+              className='w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
+            >
+              Verify Email
+            </button>
+          </form>
+        </div>
+      )}
+    </>
   )
 }
 
