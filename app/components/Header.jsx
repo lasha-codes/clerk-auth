195f019ff80c4dc520478b60a1f9c38d9c411d8b
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-21 20:04:09 +0400

    UserProfile component

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,11 @@
+import { currentUser } from '@clerk/nextjs/server'
+import { UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
+currentUser
 
-const Header = () => {
+const Header = async () => {
+  const user = await currentUser()
+  console.log(user)
   return (
     <>
       <nav className='bg-blue-700 py-4 px-6 flex items-center justify-between mb-5'>
@@ -11,13 +16,24 @@ const Header = () => {
             </div>
           </Link>
         </div>
-        <div className='text-white'>
-          <Link href='/sign-in' className='text-gray-300 hover:text-white mr-4'>
-            Sign In
-          </Link>
-          <Link href='/sign-up' className='text-gray-300 hover:text-white mr-4'>
-            Sign Up
-          </Link>
+        <div className='text-whit flex items-center'>
+          {!user && (
+            <>
+              <Link
+                href='/sign-in'
+                className='text-gray-300 hover:text-white mr-4'
+              >
+                Sign In
+              </Link>
+              <Link
+                href='/sign-up'
+                className='text-gray-300 hover:text-white mr-4'
+              >
+                Sign Up
+              </Link>
+            </>
+          )}
+          <UserButton />
         </div>
       </nav>
     </>
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className='text-2xl font-bold mb-5'>Home</h1>
+      <p className='mb-5'>
+        This is a demo site for the Traversy Media Next.js & Clerk tutorial. Go
+        ahead and sign up or sign in!
+      </p>
     </>
   )
 }
